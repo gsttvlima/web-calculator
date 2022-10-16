b9d358a0807da0b0450a3c5d09b6228ab9ba4ce1
gsttvlima <whoisgustavolima@gmail.com>
2022-10-16 20:01:07 -0300

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 import { Row, Col } from 'reactstrap';
 import { Card, CardBody, CardTitle } from 'reactstrap';
